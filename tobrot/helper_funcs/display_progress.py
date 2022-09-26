b1917da2fb76110230cb7abde6a645476caa23b0
Millionairebhai776 <105584053+Millionairebhai776@users.noreply.github.com>
2022-09-26 19:35:58 +0530

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -82,7 +82,7 @@ class Progress:
                 ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
                 round(percentage, 2))
             #cpu = "{psutil.cpu_percent()}%"
-            tmp = progress + "\n**• Total 📀:**`〘{1}〙`\n**• Done ✓ :**` 〘{0}〙`\n**• Speed 🚀 :** `〘{2}〙`\n**• ETA ⏳ :**` 〘{3}〙`".format(
+            tmp = progress + "\n**• 𝐓𝐨𝐭𝐚𝐥 ⭕️:**`〘{1}〙`\n**• 𝐃𝐨𝐧𝐞 ⚡️ :**` 〘{0}〙`\n**• 𝐒𝐩𝐞𝐞𝐝 🚀 :** `〘{2}〙`\n**• 𝐄𝐓𝐀 🌈 :**` 〘{3}〙`".format(
                 humanbytes(current),
                 humanbytes(total),
                 humanbytes(speed),
